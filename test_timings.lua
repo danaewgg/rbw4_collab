2e036a3da4fec82ad49a64bf80e753bd154e93e7
danaewgg <98764158+danaewgg@users.noreply.github.com>
2023-02-20 20:09:50 +0100

    Update test_timings.lua
    Changes

--- test_timings.lua
+++ test_timings.lua
@@ -4,7 +4,6 @@ local TeleportService = game:GetService("TeleportService")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Lighting = game:GetService("Lighting")
 local Terrain = game:GetService("Terrain")
 local VirtualUser = game:GetService("VirtualUser")
 local Players = game:GetService("Players")
@@ -28,7 +27,7 @@ if not (getgenv().boostFPS or getgenv().parts) then
     getgenv().parts = {}
 end
 
-local qsTiers = {
+local quickShotTimings = {
     [1] = 0.3, -- Bronze
     [2] = 0.6, -- Silver 
     [3] = 0.7, -- Gold
@@ -107,7 +106,8 @@ tab_Main:Toggle{
     end
 }
 
-tab_Main:TextBox{
+local textBox_Backtrack
+textBox_Backtrack = tab_Main:TextBox{
 	Name = "Backtrack",
 	Placeholder = getgenv().backtrackValue,
     Description = " [Max: 0.5] Stimulates lag, therefore increasing your ping",
@@ -116,7 +116,8 @@ tab_Main:TextBox{
 	        getgenv().backtrackValue = value
             NetworkSettings.IncomingReplicationLag = value
         else
-            Notify("[ERROR] Backtrack Cancelled", "Value too high, the max is 0.5")
+            textBox_Backtrack:Set(0)
+            notify("[ERROR] Backtrack Cancelled", "Value too high, the max is 0.5")
         end
     end
 }
@@ -137,19 +138,21 @@ tab_Main:Toggle{
                 end
                 
                 if child.Name:find("_Hoop") then
-                    for _, descendant in next, child:GetDescendants() do
-                        if descendant.Name:find("TimerDisplay") or descendant.Name:find("Slide") or descendant.Name:find("Timer Displays") or descendant.Name:find("Timer Displays") or descendant.Name:find("Net") or descendant.Name:find("Pole") or descendant.Name:find("Box") then
-                            getgenv().parts[descendant] = descendant.Parent
-                            descendant.Parent = nil
+                    for _, hoopDescendant in next, child:GetDescendants() do
+                        if hoopDescendant.Name:find("TimerDisplay") or hoopDescendant.Name:find("Slide") or hoopDescendant.Name:find("Timer Displays") or hoopDescendant.Name:find("Timer Displays") or hoopDescendant.Name:find("Net") or hoopDescendant.Name:find("Pole") or hoopDescendant.Name:find("Box") then
+                            getgenv().parts[hoopDescendant] = hoopDescendant.Parent
+                            hoopDescendant.Parent = nil
                         end
                     end
                 end
-            end
-            
-            for _, child in next, workspace.Gym.Building:GetChildren() do -- Prone to error if the place isn't the gym
-                if not child.Name:find("Light") then
-                    getgenv().parts[child] = child.Parent
-                    child.Parent = nil
+                
+                if child.Name:find("Court") then
+                    for _, courtDescendant in next, child:GetChildren() do
+                        if courtDescendant.Name:find("Baseline") then
+                            getgenv().parts[courtDescendant] = courtDescendant.Parent
+                            courtDescendant.Parent = nil
+                        end
+                    end
                 end
             end
             
@@ -157,31 +160,19 @@ tab_Main:Toggle{
                 if not child.Name:find("Building") then
                     getgenv().parts[child] = child.Parent
                     child.Parent = nil
-                end
-            end
-            
-            for _, child in next, Lighting:GetChildren() do
-                getgenv().parts[child] = child.Parent
-                child.Parent = nil
-            end
-            
-            for _, child in next, workspace.Court:GetChildren() do
-                if child.Name:find("Baseline") then
-                    getgenv().parts[child] = child.Parent
-                    child.Parent = nil
+                else -- child.Name is Building
+                    for _, buildingDescendant in next, child:GetChildren() do
+                        if not buildingDescendant.Name:find("Light") then
+                            getgenv().parts[buildingDescendant] = buildingDescendant.Parent
+                            buildingDescendant.Parent = nil
+                        end  
+                    end
                 end
             end
             
             for _, child in next, Terrain:GetChildren() do
                 getgenv().parts[child] = child.Parent
                 child.Parent = nil
-            end
-            
-            for _, child in next, ReplicatedStorage:GetChildren() do
-                if child.Name:find("DisplayCharacter") or child.Name:find("Cinematic") then
-                    getgenv().parts[child] = child.Parent
-                    child.Parent = nil
-                end
             end
 	    else
 	        getgenv().boostFPS = state
@@ -198,30 +189,33 @@ tab_Main:Button{
 	Name = "Import Timings",
 	Description = "Allows you to load timings from 'workspace/RBW4 Timings/TimingsToLoad.txt' ",
 	Callback = function()
-	    if isfile and readfile then
-    	    if isfile("RBW4 Timings/TimingsToLoad.txt") then
-                local data = {}
-
-                for _, split in next, readfile("RBW4 Timings/TimingsToLoad.txt"):split("\n") do
-                    split = string.split(split, ":")
-
-                    if split[1] and split[2] then
-                        data[trim(split[1])] = tonumber(trim(split[2]))
-                        TextBoxes[trim(split[1])]:Set(trim(split[2])) -- Set the timing textbox to the loaded value
-                    end
-                end
-                
-                getgenv().Timings = data
-
-                Notify("Imported Timings", "Timings loaded successfully")
-            else
-                Notify("[ERROR] Import Timings Cancelled", "Timings file not found")
-    	    end
-        elseif not isfile then
-            Notify("[ERROR] Import Timings Cancelled", "Your exploit does not support isfile()")
-        elseif not readfile then
-            Notify("[ERROR] Import Timings Cancelled", "Your exploit does not support readfile()")
-	    end
+        prompt("Import Timings", "Would you like to load in the saved timings?", {
+        	yes = function()
+        		if isfile and readfile then
+        			if isfile("RBW4 Timings/TimingsToLoad.txt") then -- I should make this a textbox so the user can input the exact file to load
+        				local data = {}
+        				for _, split in next, readfile("RBW4 Timings/TimingsToLoad.txt"):split("\n") do
+        					split = string.split(split, ":")
+        					if split[1] and split[2] then
+        						data[trim(split[1])] = tonumber(trim(split[2]))
+        						TextBoxes[trim(split[1])]:Set(trim(split[2])) -- Set the timing textbox to the loaded value
+        					end
+        				end
+        				getgenv().Timings = data
+        				notify("Imported Timings", "Timings loaded successfully")
+        			else
+        				notify("[ERROR] Import Timings Cancelled", "Timings file not found")
+        			end
+        		elseif not isfile then
+        			notify("[ERROR] Import Timings Cancelled", "Your exploit does not support isfile()")
+        		elseif not readfile then
+        			notify("[ERROR] Import Timings Cancelled", "Your exploit does not support readfile()")
+        		end
+        	end,
+        	no = function()
+        		return false
+        	end
+        })
     end
 }
 
@@ -237,7 +231,7 @@ tab_Main:Button{
             local currentDate = os.date("%d.%m.%Y")
             local currentTime = os.date("%H.%Mh")
 
-            local fileName = currentPing.." ping ("..currentDate.." at "..currentTime..").txt"
+            local fileName = string.format("%s ping (%s at %s)", currentPing, currentDate, currentTime)
             local fileExtension = ".txt"
             local fullPath = folderName.."/"..fileName..fileExtension
             
@@ -248,9 +242,9 @@ tab_Main:Button{
                 appendfile(fullPath, shotType.." : "..timing.."\n")
             end
             
-            Notify("Exported Timings", "File saved successfully")
+            notify("Exported Timings", "File saved successfully")
         else
-            Notify("[ERROR] Export Timings Cancelled", "Your exploit does not support writefile()")
+            notify("[ERROR] Export Timings Cancelled", "Your exploit does not support writefile()")
         end
 	end
 }
@@ -274,15 +268,23 @@ local tab_Timings = GUI:Tab{
 }
 
 task.defer(function()
-    for key, value in next, Timings do
-        TextBoxes[key] = tab_Timings:Textbox{
-            Name = key,
-            Placeholder = Timings[key],
-            Callback = function(value)
-                Timings[key] = tonumber(value)
+    local array = {}
+    for index, value in next, Timings do
+        array[#array + 1] = {index, value}
+    end
+
+    table.sort(array, function(tableA, tableB)
+        return tableA[1] < tableB[1]
+    end)
+    
+    for index, value in next, array do
+        TextBoxes[value[1]] = tab_Timings:Textbox{
+            Name = value[1],
+            Callback = function(number)
+                Timings[value[1]] = tonumber(number)
             end
         }
-    end 
+    end
 end)
 
 function trim(string)
@@ -297,7 +299,23 @@ function warn(...)
     return getgenv().warn(signature, ...)
 end
 
-function Notify(heading, description, duration)
+function prompt(title, text, buttons)
+    GUI:Prompt{
+        Followup = false, -- idk what this is tbh
+        Title = title or "Prompt",
+        Text = text or "",
+        Buttons = buttons or {
+            hi = function()
+                print("You ")
+            end,
+            no = function()
+                result = false
+            end
+        }
+    }
+end
+
+function notify(heading, description, duration)
     GUI:Notification{
     	Title = heading or "",
     	Text = description or "",
@@ -310,7 +328,7 @@ local function DisplayShotResults()
     local landedShotMeter = LocalPlayer.Character:GetAttribute("LandedShotMeter")
     local currentPing = math.round(Stats.PerformanceStats.Ping:GetValue())
     
-    Notify("Shot Results", string.format("ShotType: %s \nLandedShotMeter: %.2f \nPing: %s", shotType, landedShotMeter, currentPing))
+    notify("Shot Results", string.format("ShotType: %s \nLandedShotMeter: %.2f \nPing: %s", shotType, landedShotMeter, currentPing))
 end
 
 local function AutoRelease(shotType)
@@ -318,7 +336,7 @@ local function AutoRelease(shotType)
 		shotType = LocalPlayer.Character:GetAttribute("ShotType")
 
 		if shotType == nil then
-		    Notify("[ERROR] Auto-Release Cancelled", "ShotType is nil")
+		    notify("[ERROR] Auto-Release Cancelled", "ShotType is nil")
 			return
 		end
 	end
@@ -331,31 +349,33 @@ local function AutoRelease(shotType)
 			local function Shoot()
 			    if (tick() - startTime) >= releaseTime then
 					ReplicatedStorage.GameEvents.ClientAction:FireServer("Shoot", false)
-					-- warn(string.format("Time taken: %s", tostring(tick() - startTime)))
+					warn(string.format("Time taken: %.3f", tick() - startTime))
 					RunService:UnbindFromRenderStep("Auto-Release")
 				end
 			end
 			
+			-- This needs testing
 			if LocalPlayer.Character:GetAttribute("Streak") then
 			    if shotType == "Standing Shot" or shotType == "Off Dribble Shot" or shotType == "Hopstep Off Dribble Shot" then
 			        if LocalPlayer.Character:GetAttribute("StreakMeter") > 0 and LocalPlayer.Character:GetAttribute("StreakType") == "Spot-Up Shooter" then
-			            releaseTime = releaseTime - 0.04
+			            releaseTime = releaseTime - 0.03 -- Spot-Up timing
 			        elseif LocalPlayer.Character:GetAttribute("StreakMeter") < 0 then
-			            releaseTime = releaseTime + 0.04
+			            releaseTime = releaseTime + 0.03 -- Cold timing
 			        end
 			    end
 			end
 			
-			if LocalPlayer.PlayerGui.GameUI.Main.Boosts:FindFirstChild("Quick Shot") then
-			    if LocalPlayer.PlayerGui.GameUI.Main.Boosts["Quick Shot"]:GetAttribute("Active") then
-			        local qsTier = LocalPlayer.Character.Boosts["Quick Shot"].Value
-			        releaseTime = releaseTime - qsTiers[qsTier]
-			    end
-		    end
+			-- This needs testing
+			-- if LocalPlayer.PlayerGui.GameUI.Main.Boosts:FindFirstChild("Quick Shot") then
+			--     if LocalPlayer.PlayerGui.GameUI.Main.Boosts["Quick Shot"]:GetAttribute("Activated") then
+			--         local quickShotTier = LocalPlayer.Character.Boosts["Quick Shot"].Value
+			--         releaseTime = releaseTime - quickShotTimings[quickShotTier]
+			--     end
+		    -- end
  			
 			RunService:BindToRenderStep("Auto-Release", 1, Shoot)
 		else
-		    Notify("[ERROR] Auto-Release Cancelled", "ShotType not found in table")
+		    notify("[ERROR] Auto-Release Cancelled", "ShotType not found in table")
 		end
 	end
 end
