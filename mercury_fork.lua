9237acfbbd85761d340893be9caba290f6fea1bb
danaewgg <98764158+danaewgg@users.noreply.github.com>
2024-01-27 08:28:17 +0200

    Update mercury_fork.lua

--- mercury_fork.lua
+++ mercury_fork.lua
@@ -297,6 +297,20 @@ function Library:object(class, properties)
 
 		local hovered = false
 
+		methods.InputBegan:connect(function(input)
+			if input.UserInputType == Enum.UserInputType.Keyboard then
+				print("A key is being pushed down! Key:", input.KeyCode)
+			elseif input.UserInputType == Enum.UserInputType.MouseButton1 then
+				print("The left mouse button has been pressed down at", input.Position)
+			elseif input.UserInputType == Enum.UserInputType.MouseButton2 then
+				print("The right mouse button has been pressed down at", input.Position)
+			elseif input.UserInputType == Enum.UserInputType.Touch then
+				print("A touchscreen input has started at", input.Position)
+			elseif input.UserInputType == Enum.UserInputType.Gamepad1 then
+				print("A button is being pressed on a gamepad! Button:", input.KeyCode)
+			end
+		)
+
 		methods.MouseEnter:connect(function()
 			hovered = true
 			task.wait(0.2)
